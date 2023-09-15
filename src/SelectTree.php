a811ee36f0cf6f51efdee554478862c06511b873
Dennis Elsinga <personal.elsinga@gmail.com>
2023-09-15 10:13:23 +0200

    Added `modifyQueryUsing` closure

--- src/SelectTree.php
+++ src/SelectTree.php
@@ -7,6 +7,7 @@ use Filament\Forms\Components\Concerns\CanBeSearchable;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
 use Filament\Forms\Components\Field;
 use Illuminate\Support\Collection;
+use Closure;
 
 class SelectTree extends Field
 {
@@ -25,6 +26,8 @@ class SelectTree extends Field
     protected string $treeParentKey;
 
     protected string $titleAttribute;
+
+    protected ?Closure $modifyQueryUsing;
 
     public function withCount(bool $withCount = true): static
     {
@@ -55,25 +58,37 @@ class SelectTree extends Field
         return $this->evaluate($this->disabledBranchNode);
     }
 
-    public function tree(string $treeModel, string $treeParentKey, string $titleAttribute): static
+    public function tree(string $treeModel, string $treeParentKey, string $titleAttribute, ?Closure $modifyQueryUsing = null): static
     {
         $this->treeModel = $treeModel;
         $this->treeParentKey = $treeParentKey;
         $this->titleAttribute = $titleAttribute;
+        $this->modifyQueryUsing = $modifyQueryUsing;
 
         return $this;
     }
-
+    
     private function buildTree(int $parent = null): Collection
     {
+        // Create a default query to fetch items with the specified parent ID.
+        $defaultQuery = $this->treeModel::query()
+            ->where($this->treeParentKey, $parent);
 
-        // TEST CODE
-        $results = $this->treeModel::where($this->treeParentKey, $parent)
-            ->get();
+        // If not a root level query and a modification callback is provided, apply it.
+        if (!$parent && $this->modifyQueryUsing) {
+            $defaultQuery = $this->evaluate($this->modifyQueryUsing, ['query' => $defaultQuery]);
+        }
 
+        // Fetch the results from the default query.
+        $results = $defaultQuery->get();
+
+        // Map the results into a tree structure.
         return $results->map(function ($result) {
+
+            // Recursively build children trees for the current result.
             $children = $this->buildTree($result->id);
 
+            // Create an array representation of the current result with children.
             return [
                 'name' => $result->{$this->titleAttribute},
                 'value' => $result->id,
@@ -81,4 +96,5 @@ class SelectTree extends Field
             ];
         });
     }
+
 }
