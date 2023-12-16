5aad2bcf97c4e3f62bc06a03624812e2ef962f4d
Dennis Elsinga <personal.elsinga@gmail.com>
2023-12-16 16:31:30 +0100

    Add `HasAffixActions`

--- src/SelectTree.php
+++ src/SelectTree.php
@@ -5,19 +5,22 @@ namespace CodeWithDennis\FilamentSelectTree;
 use Closure;
 use Filament\Forms\Components\Concerns\CanBeDisabled;
 use Filament\Forms\Components\Concerns\CanBeSearchable;
+use Filament\Forms\Components\Concerns\HasActions;
 use Filament\Forms\Components\Concerns\HasAffixes;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
+use Filament\Forms\Components\Contracts\HasAffixActions;
 use Filament\Forms\Components\Field;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Collection;
 
-class SelectTree extends Field
+class SelectTree extends Field implements HasAffixActions
 {
     use CanBeDisabled;
     use CanBeSearchable;
     use HasAffixes;
     use HasPlaceholder;
+    use HasActions;
 
     protected string $view = 'select-tree::select-tree';
 
