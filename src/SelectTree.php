3bd57e87ca9b25c42822722929971d3a4e5d136b
gp-lnuff <gabriele@lineaufficio.net>
2025-07-10 09:48:28 +0200

    make append option actually do something

--- src/SelectTree.php
+++ src/SelectTree.php
@@ -437,8 +437,14 @@ class SelectTree extends Field implements HasAffixActions
 
     public function getTree(): Collection|array
     {
-        return $this->evaluate($this->buildTree()->when($this->prepend,
-            fn (Collection $tree) => $tree->prepend($this->evaluate($this->prepend))));
+        return $this->evaluate($this->buildTree()
+            ->when($this->prepend,
+            fn (Collection $tree) => $tree->prepend($this->evaluate($this->prepend))
+            )
+            ->when($this->append,
+            fn(Collection $tree) => $tree->push($this->evaluate($this->append))
+            )
+        );
     }
 
     public function getResults(): Collection|array|null
